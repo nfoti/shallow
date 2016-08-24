d0ec683c27417ffc779271552852082a60dc9419
wronk <wronk.mark@gmail.com>
2016-08-24 16:45:26 -0700

    Add functionality to simulate data

--- scratch/shallow.py
+++ scratch/shallow.py
@@ -8,7 +8,7 @@
       shallow.py (-h | --help)
 
     Options:
-      --subj=<subj>     Specify subject to process
+      --subj=<subj>     Specify subject to process with structural name
       -h, --help        Show this screen
 """
 from __future__ import absolute_import
@@ -19,6 +19,7 @@ import os
 import os.path as op
 
 import mne
+from mne import SourceEstimate
 from mne.minimum_norm import read_inverse_operator
 from mne.simulation import simulate_sparse_stc, simulate_stc, simulate_evoked
 from mne.externals.h5io import read_hdf5, write_hdf5
@@ -49,41 +50,40 @@ def load_subject_objects(megdatadir, subj, struct):
     print("  %s: -- loading meg objects" % subj)
 
     fname_fwd = op.join(megdatadir, subj, 'forward',
-                         '%s-sss-fwd.fif' % subj)
-    fwd = mne.read_forward_solution(fname_fwd, force_fixed=True,
-            surf_ori=True)
+                        '%s-sss-fwd.fif' % subj)
+    fwd = mne.read_forward_solution(fname_fwd, force_fixed=True, surf_ori=True)
 
     fname_inv = op.join(megdatadir, subj, 'inverse',
-                         '%s-55-sss-meg-eeg-fixed-inv.fif' % subj)
+                        '%s-55-sss-meg-eeg-fixed-inv.fif' % subj)
     inv = read_inverse_operator(fname_inv)
-    
+
     fname_epochs = op.join(megdatadir, subj, 'epochs',
-                            'All_55-sss_%s-epo.fif' % subj)
+                           'All_55-sss_%s-epo.fif' % subj)
     #epochs = mne.read_epochs(fname_epochs)
     #evoked = epochs.average()
     #evoked_info = evoked.info
     evoked_info = mne.io.read_info(fname_epochs)
-    #cov = mne.compute_covariance(epochs, tmin=None, tmax=0.)
-    fname_cov = op.join(megdatadir, subj, 'cov',
-                        'All_55-sss_%s-empirical-cov.fif' % subj)
-    cov = mne.read_cov(fname_cov)
+    cov = inv['noise_cov']
 
     return subj, fwd, inv, cov, evoked_info
 
 
-def gen_evoked_subject(signal, fwd, cov, evoked_info, dt, noise_snr):
+def gen_evoked_subject(signal, fwd, cov, evoked_info, dt, noise_snr,
+                       seed=None):
+    """Function to generate evoked and stc from signal array"""
 
-    stc = simulate_stc(fwd['src'], labels_subj, signal, times[0],
-            dt, value_fun=lambda x: x)
-    
+    vertices = [fwd['src'][0]['vertno'], fwd['src'][1]['vertno']]
+    stc = SourceEstimate(sim_vert_data, vertices, tmin=0, tstep=dt)
+
     evoked = simulate_evoked(fwd, stc, evoked_info, cov, noise_snr,
-            tmin=0., tmax=1., random_state=seed)
+                             random_state=seed)
     evoked.add_eeg_average_proj()
-    
+
     return evoked, stc
 
 
 def make_network():
+    """Create neural network"""
     net = None
     return net
 
@@ -102,17 +102,26 @@ if __name__ == "__main__":
         struct = argv['--subj']
         subj = subjects[structurals.index(struct)]
 
+    # Params
+    n_times = 250
+    dt = 0.001
+    noise_snr = np.inf
+
+    # Get subject info and create data
     subj, fwd, inv, cov, evoked_info = load_subject_objects(megdir, subj,
                                                             struct)
+    n_verts = fwd['src'][0]['nuse'] + fwd['src'][1]['nuse']
+    sim_vert_data = np.random.randn(n_verts, n_times)
 
-    #nverts = fwd['src'].n_vertices
-    #T = 250
-    #signal = np.random.randn()
-    #evoked, stc = gen_evoked_subject(
+    evoked, stc = gen_evoked_subject(sim_vert_data, fwd, cov, evoked_info, dt,
+                                     noise_snr)
 
+    # Create neural network
     # net = make_net()
 
     # Train net
     #   Make data iterator for tensorflow
+    #   Save checkpoints after training each batch
+    #   Visualize training loss
 
-    # evaluate net
+    # Evaluate net
