5700b0aef7d6c55d7c69eb080f765923d9b22916
wronk <wronk.mark@gmail.com>
2016-09-14 16:27:03 -0700

    Functionalize several things, add sparse activation data

--- scratch/shallow.py
+++ scratch/shallow.py
@@ -20,6 +20,7 @@ import os.path as op
 
 import tensorflow as tf
 import numpy as np
+from scipy.sparse import random as random_sparse
 
 import mne
 from mne import SourceEstimate
@@ -30,7 +31,7 @@ from mne.externals.h5io import read_hdf5, write_hdf5
 from shallow_fun import (load_subject_objects, gen_evoked_subject,
                          get_data_batch, get_all_vert_positions,
                          get_largest_dip_positions, get_localization_metrics,
-                         eval_error_norm)
+                         eval_error_norm, norm_transpose)
 
 # Entries in structurals and subjects must correspoond,
 # i.e. structurals[i] === subjects[i].
@@ -79,19 +80,31 @@ def make_tanh_network(sensor_dim, source_dim):
     b2 = bias_variable([source_dim // 2], name='b2')
     h2 = tf.nn.tanh(tf.matmul(h1, W2) + b2)
 
-    W3 = weight_variable([source_dim // 2, source_dim], name='W3')
-    b3 = bias_variable([source_dim], name='b3')
-    yhat = tf.nn.tanh(tf.matmul(h2, W3) + b3)
+    W3 = weight_variable([source_dim // 2, source_dim // 4], name='W3')
+    b3 = bias_variable([source_dim // 4], name='b3')
+    h3 = tf.nn.tanh(tf.matmul(h2, W3) + b3)
+
+    W4 = weight_variable([source_dim // 4, source_dim], name='W4')
+    b4 = bias_variable([source_dim], name='b4')
+    yhat = tf.nn.tanh(tf.matmul(h3, W4) + b4)
 
     # Attach histogram summaries to weight functions
     tf.histogram_summary('W1 Hist', W1)
     tf.histogram_summary('W2 Hist', W2)
     tf.histogram_summary('W3 Hist', W3)
+    tf.histogram_summary('W4 Hist', W4)
 
-    return yhat, h1, h2, x_sensor
+    h_list = [h1, h2, h3]
+
+    return yhat, h_list, x_sensor
 
 
-def sparse_objective(sensor_dim, source_dim, yhat, h1, h2, sess):
+def bernoulli(a_obj, rho):
+    return (rho * (tf.log(rho) - tf.log(a_obj + 1e-6) + (1 - rho) *
+                   (tf.log(1 - rho) - tf.log(1 - a_obj + 1e-6))))
+
+
+def sparse_objective(sensor_dim, source_dim, yhat, h_list, sess):
 
     y_source = tf.placeholder(tf.float32, shape=[None, source_dim], name="y_source")
     rho = tf.placeholder(tf.float32, shape=(), name="rho")
@@ -101,15 +114,12 @@ def sparse_objective(sensor_dim, source_dim, yhat, h1, h2, sess):
     error = tf.reduce_sum(tf.squared_difference(y_source, yhat))
 
     # Remap activations to [0,1]
-    a1 = 0.5 * h1 + 0.5
-    a2 = 0.5 * h2 + 0.5
+    a_list = [0.5 * h_obj + 0.5 for h_obj in h_list]
 
-    kl_bernoulli_h1 = (rho * (tf.log(rho) - tf.log(a1 + 1e-6) + (1 - rho) *
-                              (tf.log(1 - rho) - tf.log(1 - a1 + 1e-6))))
-    kl_bernoulli_h2 = (rho * (tf.log(rho) - tf.log(a2 + 1e-6) + (1 - rho) *
-                              (tf.log(1 - rho) - tf.log(1 - a2 + 1e-6))))
-    regularizer = (tf.reduce_sum(kl_bernoulli_h1)
-                   + tf.reduce_sum(kl_bernoulli_h2))
+    kl_bernoulli_list = [bernoulli(a_obj, rho) for a_obj in a_list]
+
+    regularizer = sum([tf.reduce_sum(kl_bernoulli_h)
+                       for kl_bernoulli_h in kl_bernoulli_list])
 
     cost = error + lam * regularizer
 
@@ -134,42 +144,63 @@ if __name__ == "__main__":
         subj = subjects[structurals.index(struct)]
 
     # Set params
-    n_data_times = 50000
-    dt = 0.001
-    noise_snr = np.inf
-    batch_size = 1000
+    n_training_times_noise = 10000
+    n_training_times_sparse = 10000
+    n_training_times = n_training_times_noise + n_training_times_sparse
 
-    n_iter = int(500000)
+    dt = 0.001
+    SNR = np.inf
+    batch_size = 500
+
+    n_iter = int(5000)
     rho = 0.05
     lam = 1.
 
     save_network = True
     fpath_save = op.join('model_subj_{}_iters.meta'.format(n_iter))
+    n_training_times = n_training_times_noise + n_training_times_sparse
 
     # Get subject info and create data
     subj, fwd, inv, cov, evoked_info = load_subject_objects(megdir, subj,
                                                             struct)
-    n_verts = fwd['src'][0]['nuse'] + fwd['src'][1]['nuse']
-    sim_vert_data = np.random.randn(n_verts, n_data_times)
+    sensor_dim = len(fwd['info']['ch_names'])
+    source_dim = fwd['src'][0]['nuse'] + fwd['src'][1]['nuse']
 
-    print("Simulating and normalizing data")
-    evoked, stc = gen_evoked_subject(sim_vert_data, fwd, cov, evoked_info, dt,
-                                     noise_snr)
-    # Normalize data to lie between -1 and 1
-    x_sens_all = np.ascontiguousarray(evoked.data.T)
-    x_sens_all /= np.max(np.abs(x_sens_all))
-    y_src_all = np.ascontiguousarray(sim_vert_data.T)
-    y_src_all /= np.max(np.abs(y_src_all))
+    sparse_dens = 1. / source_dim  # Density of non-zero vals in sparse training data
+    sparse_dist = np.random.randn
+
+    noise_data = np.random.randn(source_dim, n_training_times_noise)
+    sparse_data = random_sparse(source_dim, n_training_times_sparse,
+                                sparse_dens, random_state=0,
+                                dtype=np.float32, data_rvs=sparse_dist).toarray()
+    sim_train_data = np.concatenate((noise_data, sparse_data), axis=1)
+
+    print("Simulating and normalizing training data")
+    evoked, stc = gen_evoked_subject(sim_train_data, fwd, cov, evoked_info, dt,
+                                     SNR)
+    # Normalize training data to lie between -1 and 1
+    x_train = norm_transpose(evoked.data)
+    y_train = norm_transpose(sim_train_data)
+
+    """
+    print("Simulating and normalizing testing data")
+    evoked, stc = gen_evoked_subject(sim_test_data, fwd, cov, evoked_info, dt,
+                                     SNR)
+    # Normalize testing data to lie between -1 and 1
+    x_test = norm_transpose(evoked.data)
+    y_test = norm_transpose(sim_vert_data)
+
+    val_monitor = tf.contrib.learn.monitors.ValidationMonitor(
+        x_test, y_test, every_n_steps=20)
+    """
 
     print("Building neural net")
     sess = tf.Session()
 
-    sensor_dim = evoked.data.shape[0]
-    source_dim = n_verts
-    yhat, h1, h2, x_sensor = make_tanh_network(sensor_dim, source_dim)
+    yhat, h_list, x_sensor = make_tanh_network(sensor_dim, source_dim)
     sparse_cost, y_source, tf_rho, tf_lam = sparse_objective(sensor_dim,
                                                              source_dim, yhat,
-                                                             h1, h2, sess)
+                                                             h_list, sess)
     merged_summaries = tf.merge_all_summaries()
     train_writer = tf.train.SummaryWriter('./train_summaries', sess.graph)
 
@@ -177,19 +208,20 @@ if __name__ == "__main__":
 
     saver = tf.train.Saver()
     sess.run(tf.initialize_all_variables())
-    print("\nSim params\n----------\nn_iter: {}\nn_data_points: {}\nSNR: \
-          {}\nbatch_size: {}\n".format(n_iter, n_data_times, str(noise_snr),
-                                       batch_size))
+    print("\nSim params\n----------\nn_iter: {}\nn_training_times: {}\nSNR: \
+          {}\nbatch_size: {}\n".format(n_iter, n_training_times,
+                                       str(SNR), batch_size))
     print("Optimizing...")
     for ii in xrange(n_iter):
         # Get random batch of data
-        x_sens_batch, y_src_batch = get_data_batch(x_sens_all, y_src_all, ii,
+        x_sens_batch, y_src_batch = get_data_batch(x_train, y_train, ii,
                                                    batch_size)
 
         # Take training step
         feed_dict = {x_sensor: x_sens_batch, y_source: y_src_batch,
                      tf_rho: rho, tf_lam: lam}
 
+        # Save summaries for tensorboard every 10 steps
         if ii % 10 == 0:
             _, obj, summary = sess.run([train_step, sparse_cost,
                                         merged_summaries], feed_dict)
