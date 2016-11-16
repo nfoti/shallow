becd44bc55b3e84396ff9e76bae6de403bc0d44b
wronk <wronk.mark@gmail.com>
2016-11-15 16:32:56 -0800

    Use config file, rename vars for clarity

--- scratch/eval_model.py
+++ scratch/eval_model.py
@@ -4,8 +4,8 @@
     signals. Tests localization error and point spread
 
     Usage:
-      shallow.py <megdir> <structdir> <nn_fname> [--subj=<subj>]
-      shallow.py (-h | --help)
+      eval_model.py <megdir> <structdir> <nn_fname> [--subj=<subj>]
+      eval_model.py (-h | --help)
 
     Options:
       --subj=<subj>     Specify subject to process with structural name
@@ -31,22 +31,11 @@ from shallow_fun import (load_subject_objects, gen_evoked_subject,
                          get_localization_metrics, eval_error_norm,
                          norm_transpose)
 from shallow import make_tanh_network, sparse_objective
-
-
-# Entries in structurals and subjects must correspoond,
-# i.e. structurals[i] === subjects[i].
-structurals = ['AKCLEE_103', 'AKCLEE_104', 'AKCLEE_105', 'AKCLEE_106',
-               'AKCLEE_107', 'AKCLEE_109', 'AKCLEE_110', 'AKCLEE_115',
-               'AKCLEE_117', 'AKCLEE_118', 'AKCLEE_119', 'AKCLEE_121',
-               'AKCLEE_125', 'AKCLEE_126', 'AKCLEE_131', 'AKCLEE_132']
-subjects = ['eric_sps_03', 'eric_sps_04', 'eric_sps_05', 'eric_sps_06',
-            'eric_sps_07', 'eric_sps_09', 'eric_sps_10', 'eric_sps_15',
-            'eric_sps_17', 'eric_sps_18', 'eric_sps_19', 'eric_sps_21',
-            'eric_sps_25', 'eric_sps_26', 'eric_sps_31', 'eric_sps_32']
+from config import common_params, eval_params, conf_structurals, conf_subjects
 
 # Removing eric_sps_32/AKCL_132 b/c of vertex issue
-structurals = structurals[:-1]
-subjects = subjects[:-1]
+structurals = conf_structurals[:-1]
+subjects = conf_subjects[:-1]
 
 
 if __name__ == "__main__":
@@ -64,13 +53,10 @@ if __name__ == "__main__":
         struct = argv['--subj']
         subj = subjects[structurals.index(struct)]
 
-    # Set params
-    dt = 0.001
-    noise_snr = np.inf
-    rho = 0.05
-    lam = 1.
-    n_avg_verts = 25  # Number of verts to avg when determining est position
-    n_test_verts = 1000  # Probably should be <= 1000 to avoid mem problems
+    # Number of verts to avg when determining est position
+    n_avg_verts = eval_params['n_avg_verts']
+    # Probably should be <= 1000 to avoid mem problems
+    n_test_verts = eval_params['n_test_verts']
 
     sess = tf.Session()
 
@@ -86,22 +72,26 @@ if __name__ == "__main__":
 
     print("Reconstructing model and restoring saved weights")
     # Reconstruct network
-    yhat, h_list, x_sensor = make_tanh_network(sensor_dim, source_dim)
+    network_dims = [source_dim // 2, source_dim // 2, source_dim]
+    yhat, h_list, x_sensor = make_tanh_network(sensor_dim, source_dim, network_dims)
     sparse_cost, y_source, tf_rho, tf_lam = sparse_objective(sensor_dim, source_dim,
                                                              yhat, h_list,
                                                              sess)
     saver = tf.train.Saver()
-    saver.restore(sess, 'model_AKCLEE_107')
+    saver.restore(sess, model_fname)
 
     print("\nEvaluating deep learning approach...\n")
 
     # Simulate unit dipole activations
-    rand_verts = np.sort(np.random.randint(0, n_verts, n_test_verts))
-    sim_vert_data = np.eye(n_verts)[:, rand_verts]
-    evoked, stc = gen_evoked_subject(sim_vert_data, fwd, cov, evoked_info, dt,
-                                     noise_snr)
 
-    #TODO: normalize data? Transfer normalization multipliers? (maybe with sklearn)
+    rand_verts = np.sort(np.random.choice(range(n_verts), n_test_verts,
+                                          replace=False))
+    sim_vert_data = np.eye(n_verts)[:, rand_verts]
+    evoked, stc = gen_evoked_subject(sim_vert_data, fwd, cov, evoked_info,
+                                     common_params['dt'],
+                                     common_params['SNR'])
+
+    # Normalize data and transpose so it's (n_observations x n_chan)
     x_test = norm_transpose(evoked.data)
     y_test = norm_transpose(stc.data)
 
@@ -109,10 +99,11 @@ if __name__ == "__main__":
     vert_positions = get_all_vert_positions(inv['src'])
     true_act_positions = vert_positions[rand_verts, :]
 
-    feed_dict = {x_sensor: x_test, y_source: y_test, tf_rho: rho, tf_lam: lam}
+    feed_dict = {x_sensor: x_test, y_source: y_test,
+                 tf_rho: common_params['rho'], tf_lam: common_params['lam']}
     src_est_dl = sess.run(yhat, feed_dict)
     stc_dl = SourceEstimate(src_est_dl.T, vertices=vert_list, subject=struct,
-                            tmin=0, tstep=0.001)
+                            tmin=0, tstep=common_params['dt'])
 
     # Calculate vector norm error
     error_norm_dl = eval_error_norm(y_test, src_est_dl)
@@ -127,23 +118,22 @@ if __name__ == "__main__":
     #
     # Evaluate standard MNE methods
     #
-    stc_mne = apply_inverse(evoked, inv, method='sLORETA')
-    src_est_mne = stc_mne.data.T
+    stc_std = apply_inverse(evoked, inv, method=eval_params['linear_inv'])
+    src_est_std = stc_std.data.T
 
     # Calculate vector norm error
-    error_norm_mne = eval_error_norm(y_test, src_est_mne)
-    est_act_positions = get_largest_dip_positions(src_est_mne, n_avg_verts,
+    error_norm_std = eval_error_norm(y_test, src_est_std)
+    est_act_positions = get_largest_dip_positions(src_est_std, n_avg_verts,
                                                   vert_positions)
-    accuracy_mne, point_spread_mne = get_localization_metrics(true_act_positions,
+    accuracy_std, point_spread_std = get_localization_metrics(true_act_positions,
                                                               est_act_positions)
 
-    '''
-    print('\bDeep learning; error norm average for {} verts: {:0.4f}'.format(
+    sess.close()
+    print('\bShallow; error norm average for {} verts: {:0.4f}'.format(
         n_test_verts, np.mean(error_norm_dl)))
     print('Linear method: error norm average for {} verts: {:0.4f}\n'.format(
-        n_test_verts, np.mean(error_norm_mne)))
-        '''
-    print('Deep learning; Accuracy: {:0.5f}, Avg. Point spread: {:0.5f}'.format(
+        n_test_verts, np.mean(error_norm_std)))
+    print('Shallow; Loc. accuracy: {:0.5f}, Avg. Point spread: {:0.5f}'.format(
         accuracy_dl, np.mean(point_spread_dl)))
-    print('Linear method; Accuracy: {:0.5f}, Avg. Point spread: {:0.5f}\n'.format(
-        accuracy_mne, np.mean(point_spread_mne)))
+    print('Linear method; Loc. accuracy: {:0.5f}, Avg. Point spread: {:0.5f}\n'.format(
+        accuracy_std, np.mean(point_spread_std)))
